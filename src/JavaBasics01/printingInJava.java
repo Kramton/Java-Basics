964b10c75fd937235efc00da787081f3bafe0f40
Mark <test@email.com>
2023-10-24 20:26:22 +1300

    Printing In Java

---
+++ src/JavaBasics01/printingInJava.java
@@ -0,0 +1,13 @@
+package JavaBasics01;
+
+public class printingInJava 
+{
+	public static void main(String[] args)
+	{
+		// Print in one line 
+		System.out.print("Hello World!");
+		
+		// Print in another line
+		System.out.println("Hello World! In the next line.");
+	}
+}
