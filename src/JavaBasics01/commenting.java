0d19c5af097e9880ac7e285afd04e8759cb7a6f0
Mark <test@email.com>
2023-10-24 20:35:28 +1300

    Created Commenting in Java file

---
+++ src/JavaBasics01/commenting.java
@@ -0,0 +1,18 @@
+package JavaBasics01;
+
+public class commenting 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is a sentence...");
+		
+		//System.out.println("This is a commented sentence...")
+		
+		/*
+		 * This is another way to comment.
+		 *
+		 */
+	}
+
+}
