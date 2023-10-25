aa05b3e28d84f96aa34b7364e56c1b19bc984286
Mark <test@email.com>
2023-10-25 15:42:59 +1300

    Create EscapeSequences.java

---
+++ src/JavaBasics01/EscapeSequences.java
@@ -0,0 +1,12 @@
+package JavaBasics01;
+
+public class EscapeSequences 
+{
+	public static void main(String[] args) 
+	{
+		// You can print " and \ by adding a backslash.
+		System.out.println("Hello \"Mark\"");
+		System.out.println("This is a backslash \\");
+		System.out.println("Tabbing a sentence \t right here! \n\nAdding 2 new lines here...");
+	}
+}
