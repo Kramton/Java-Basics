3f1be1423ee17e41b57fb828e596ad6546422e1f
Mark <test@email.com>
2023-10-25 19:58:25 +1300

    Create LongFloatDouble.java

---
+++ src/JavaBasics01/LongFloatDouble.java
@@ -0,0 +1,17 @@
+package JavaBasics01;
+
+public class LongFloatDouble 
+{
+	public static void main(String[] args)
+	{
+		int number1 = 7;
+		long number2 = 123456789l;
+		float number3 = 2.1f;
+		double number4 = 433.9439;
+		
+		System.out.println(number1);
+		System.out.println(number2);
+		System.out.println(number3);
+		System.out.println(number4);
+	}
+}
