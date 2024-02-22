db6c7d9944233e3baad39be3fb1ee29fd2c50e27
Mark <test@email.com>
2024-02-22 17:05:59 +1300

    Create IfElse.java

---
+++ src/JavaBasics01/IfElse.java
@@ -0,0 +1,40 @@
+package JavaBasics01;
+
+import java.util.Scanner;
+
+public class IfElse 
+{
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter a grade: ");
+		
+		int grade = scan.nextInt();
+		
+		if(grade >= 90 && grade <= 100)
+		{
+			System.out.println("A");
+		}
+		else if(grade >= 80 && grade < 90)
+		{
+			System.out.println("B");
+		}
+		else if(grade >= 70 && grade < 80)
+		{
+			System.out.println("C");
+		}
+		else if(grade >= 60 && grade < 70)
+		{
+			System.out.println("D");
+		}
+		else if(grade >= 0 && grade < 60)
+		{
+			System.out.println("F");
+		}
+		else
+		{
+			System.out.println("Grade not in range...");
+		}
+		
+	}
+}
