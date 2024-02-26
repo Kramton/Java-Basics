ca851955d475b6b6f58371bdf37ce0f57f15f004
Mark <test@email.com>
2024-02-26 18:53:47 +1300

    Update NestedForLoops.java

--- src/JavaBasics01/NestedForLoops.java
+++ src/JavaBasics01/NestedForLoops.java
@@ -4,16 +4,29 @@ public class NestedForLoops
 {
 	public static void main(String[] args) 
 	{
-		for(int i = 0; i < 5; i++)
-		{
-			System.out.println("Hello World!");
-		}
-		
-		String[] colors = {"Red", "Blue", "Green"};
+		String[] colors = {"Red", "Green", "Blue"};
 		
 		for(int i = 0; i < colors.length; i++)
 		{
 			System.out.println("Colors: " + colors[i]);
+		}
+		
+		String[][] fancyColors = {{"Red", "Green", "Blue"}, {"Violet", "Cyan", "Magenta"}};
+		
+		for(int rows = 0; rows < fancyColors.length; rows++)
+		{
+			for(int column = 0; column < 3; column++)
+			{
+				System.out.println(fancyColors[rows][column]);
+			}
+		}
+		
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 0; j < 10; j++)
+			{
+				System.out.println("i: " + i + ", j: " + j);
+			}
 		}
 	}
 }
