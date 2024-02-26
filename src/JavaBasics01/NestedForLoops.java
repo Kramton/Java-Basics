950c9b61c8b74e0818e37038413a016726baa344
Mark <test@email.com>
2024-02-26 17:53:52 +1300

    Update NestedForLoops.java

--- src/JavaBasics01/NestedForLoops.java
+++ src/JavaBasics01/NestedForLoops.java
@@ -4,6 +4,16 @@ public class NestedForLoops
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello");
+		for(int i = 0; i < 5; i++)
+		{
+			System.out.println("Hello World!");
+		}
+		
+//		String[] colors = {"Red", "Blue", "Green"};
+		
+//		for(int i = 0; i < colors.length; i++)
+//		{
+//			System.out.println("Colors: " + colors[i]);
+//		}
 	}
 }
