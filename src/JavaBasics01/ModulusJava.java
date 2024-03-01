3782761f9310ff6c77241d18146c262a6e6fec8e
Mark <test@email.com>
2024-03-01 14:59:46 +1300

    Create ModulusJava.java

---
+++ src/JavaBasics01/ModulusJava.java
@@ -0,0 +1,10 @@
+package JavaBasics01;
+
+public class ModulusJava 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(15 % 4); // 4 goes into 15 3 times so 12 remainder 3
+		System.out.println(1 % 0.6); // 0.6 goes into 1 1 time so 0.6 remainder 0.4
+	}
+}
